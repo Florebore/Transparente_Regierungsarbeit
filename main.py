8da1f53353b73025bf0b737b31643425a6cc99ad
Florebore <florian.boettinger@gmail.com>
2024-11-04 11:50:32 +0100

    JS works now

---
+++ main.py
@@ -0,0 +1,11 @@
+from pyscript import document
+
+def se(self):
+   div_c = document.getElementById("result")
+   div_c.innerText = "Hide"
+   button_c = document.getElementById("search_button")
+   button_c.innerText = "Jetzt"
+
+
+
+
