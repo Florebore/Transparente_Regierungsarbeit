8641dc796ed5d445fd53857f0c6f7bf584a22133
Florebore <florian.boettinger@gmail.com>
2024-12-27 16:40:01 +0100

    Matplotlib funktioniert, html code muss in div element eine Referenz auf main.py enthalten

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.tri as tri
+import matplotlib.dates as mdates
 from pyscript import document
 from pyscript import display
+import json
+import urllib.request
 
 
 def se(self):
@@ -10,33 +13,20 @@ def se(self):
     button_c = document.getElementById("plot-button")
     button_c.innerText = "Jetzt"
     # First create the x and y coordinates of the points.
-    n_angles = 36
-    n_radii = 8
-    min_radius = 0.25
-    radii = np.linspace(min_radius, 0.95, n_radii)
+    fig_party_survey = plt.figure()
+    fig_party_survey, ax = plt.subplots()
+    ax.plot([10,20,30,40,50,60,70,80,90,100])
+    ax.set_title("Umfragewerte der Parteien")
+    ax.set_xlabel("Zeitverlauf")
+    ax.set_ylabel("Prozent")
+    ax.legend()
+    display(fig_party_survey, target="party_survey_mpl")
 
-    angles = np.linspace(0, 2 * np.pi, n_angles, endpoint=False)
-    angles = np.repeat(angles[..., np.newaxis], n_radii, axis=1)
-    angles[:, 1::2] += np.pi / n_angles
-
-    x = (radii * np.cos(angles)).flatten()
-    y = (radii * np.sin(angles)).flatten()
-    z = (np.cos(radii) * np.cos(3 * angles)).flatten()
-
-    # Create the Triangulation; no triangles so Delaunay triangulation created.
-    triang = tri.Triangulation(x, y)
-
-    # Mask off unwanted triangles.
-    triang.set_mask(np.hypot(x[triang.triangles].mean(axis=1),
-                             y[triang.triangles].mean(axis=1))
-                    < min_radius)
-
-    fig1, ax1 = plt.subplots()
-    ax1.set_aspect('equal')
-    tpc = ax1.tripcolor(triang, z, shading='flat')
-    fig1.colorbar(tpc)
-    ax1.set_title('tripcolor of Delaunay triangulation, flat shading')
-
-    display(fig1, target="party_survey_mpl")
+def convert_json(String json):
 
 
+
+
+def get_request(String http):
+
+    contents = urllib.request.urlopen("http://example.com/foo/bar").read()
