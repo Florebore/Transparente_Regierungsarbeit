b0849a82a5ed34832148d7b3b264f9f65b1bb6e7
Florebore <florian.boettinger@gmail.com>
2024-12-27 12:05:43 +0100

    Matplotlib funktioniert, html code muss in div element eine Referenz auf main.py enthalten

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.tri as tri
 from pyscript import document
+from pyscript import display
+
 
 def se(self):
-   div_c = document.getElementById("result")
-   div_c.innerText = "Hide"
-   button_c = document.getElementById("search_button")
-   button_c.innerText = "Jetzt"
+    print("Ja")
+    button_c = document.getElementById("plot-button")
+    button_c.innerText = "Jetzt"
+    # First create the x and y coordinates of the points.
+    n_angles = 36
+    n_radii = 8
+    min_radius = 0.25
+    radii = np.linspace(min_radius, 0.95, n_radii)
 
+    angles = np.linspace(0, 2 * np.pi, n_angles, endpoint=False)
+    angles = np.repeat(angles[..., np.newaxis], n_radii, axis=1)
+    angles[:, 1::2] += np.pi / n_angles
 
+    x = (radii * np.cos(angles)).flatten()
+    y = (radii * np.sin(angles)).flatten()
+    z = (np.cos(radii) * np.cos(3 * angles)).flatten()
+
+    # Create the Triangulation; no triangles so Delaunay triangulation created.
+    triang = tri.Triangulation(x, y)
+
+    # Mask off unwanted triangles.
+    triang.set_mask(np.hypot(x[triang.triangles].mean(axis=1),
+                             y[triang.triangles].mean(axis=1))
+                    < min_radius)
+
+    fig1, ax1 = plt.subplots()
+    ax1.set_aspect('equal')
+    tpc = ax1.tripcolor(triang, z, shading='flat')
+    fig1.colorbar(tpc)
+    ax1.set_title('tripcolor of Delaunay triangulation, flat shading')
+
+    display(fig1, target="party_survey_mpl")
 
 
