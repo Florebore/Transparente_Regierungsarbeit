ea5d420907d7cadbb791672b8fed876cdc31a821
Florebore <florian.boettinger@gmail.com>
2025-01-20 21:15:54 +0100

    Changelog: 1.Matplotlib funktioniert, html code muss in div element eine Referenz auf main.py enthalten, 2.http_handler mit Auslesen von Json funktioniert, weitere Funktionen zum Auslesen der Werte aus dem JSON, mehr Werte werden ausgelesen 3.Mit Hilfe der Pandas library wird der Durchschnitt aller Umfragewerte aus den Daten ausgelesen 4. data_handler erstellt, um http-Service und Daten zu separieren -> Data handler muss separiert werden in einzelne Anfragen 5. Plotly funktioniert auch 6. Parteien zu panda.Dataframe hinzugefügt / Problem mit packages in pyscript.json gelöst 7. DataFrame ist richtig formatiert und plottet jetzt Ergebnisse richtig 8. Methode zum extrahieren der Umfrageinstitute implementiert 9. Daten zu DataFrame hinzugefügt
    Nächste ToDos
    
    1. Auswahl der Visualisierungslibrary Matrplotlib vs Plotly -> interaktive Daten?
    2. Einfügen der Daten in die jeweiligen Charts -> Plotly und Matplotlib
    3. weitere Extraktion von Daten aus den Umfrageergebnissen nötig
    4. Zeitverlauf der Umfrage und Umfrageinstitute adden / Plot konfigurieren / Unklar wir die Institute in den Dataframe eingefügt werden können

--- data_handler.py
+++ data_handler.py
@@ -34,10 +34,6 @@ class data_handler:
         institute_dictionary = dict(zip(x,y))
         print(institute_dictionary)
 
-
-
-
-
     @staticmethod
     def extract_mean_of_survey(self):
 
@@ -72,43 +68,42 @@ class data_handler:
         for i in range(0, umfrage_anzahl - 1):
             if umfrage_data.get("Surveys").get(survey_dict_keys[i]).get("Parliament_ID") == "0":
                 print(survey_dict.get(survey_dict_keys[i]).get("Results"))
+                print(survey_dict.get(survey_dict_keys[i]).get("Date"))
                 #fügt alle aktuellen Umfragen als Dictionary in eine Liste
                 bundestag_surveys.append(survey_dict.get(survey_dict_keys[i]).get("Results"))
                 #fügt das Institut, das Umfragedatum und die Umfragemethode in eine separate Liste
-                #bundestag_surveys_additional_info.append(survey_dict.get(survey_dict_keys[i]).get("Date"))
+                bundestag_surveys_additional_info.append(survey_dict.get(survey_dict_keys[i]).get("Date"))
                 print(bundestag_surveys)
-        # sum of surveys / test of sum function / not necessary anymore becsause of mean in PandaDataframe
-        #sum = bundestag_surveys
-        #c = Counter()
-        #for d in sum:
-        #    c.update(d)
-        #print(c)
+                print(bundestag_surveys_additional_info)
+
+        #add dates to data
+        bundestag_surveys_date = dict(zip(bundestag_surveys_additional_info,bundestag_surveys))
+        print(bundestag_surveys_date)
+        df1 = pd.DataFrame(bundestag_surveys_date)
+
+        #label lines with parties
+        p = df1_line_list = df1.index.to_list()
+        e: int
+        for e in range (0,len(df1_line_list)):
+            df1.rename(index={df1_line_list[e]:umfrage_data.get("Parties").get(df1_line_list[e]).get("Name")}, inplace=True)
 
         # average of surveys with pandas / The result list of JSON from Dawum is
         df = pd.DataFrame(bundestag_surveys)
 
         #answer ist das Mittel aller Abfragen
         answer = dict(df.mean())
-        print(answer)
 
         #label columns with Party names
         k = df_columns_list = df.columns.to_list()
-        print(df_columns_list[1])
-        print(int(df_columns_list[3]))
-        print(umfrage_data.get("Parties").get(df_columns_list[3]).get("Shortcut"))
-
-        print(umfrage_data.get("Parties").get(party_dict_keys[1]).get("Name"))
         c: int
         for c in range(0,len(df_columns_list)):
             df.rename(columns={df_columns_list[c]:umfrage_data.get("Parties").get(df_columns_list[c]).get("Name")}, inplace=True)
-
+        print(df1)
         print(df)
         print(df.columns.values)
         print(answer)
 
         return df
-    #add correct names of Parties to survey
-    #def party_label(self,pd.DataFrame):
 
 
 i = data_handler("mean")
